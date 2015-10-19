4b8f8ac4c88a68ec7f1efca8d3ec8af214692f5b
sfujikawa <sfujikawa.itfor@gmail.com>
2015-10-19 16:34:30 +0900

    Gitテスト
    netbeansからコミットするテスト

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+    
+        System.out.println("hallo word");
+        
+        //ラムダ式
+        //関数を簡略化して記載する。
+        //List<String> 
+        
+        
+        //ストリームAPI
+        //ラムダ式を利用したコレクション操作用のAPI
+        
+        
+        
+    }
+}
